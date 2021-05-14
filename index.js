2bbefc3db26203f7e2dd1390a322463be6168863
bharatrajput2409 <nutboltcode@gmail.com>
2021-05-14 14:51:52 +0530

    temp

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const bodyParse = require('body-parser')
 const port = process.env.port || 5000;
 
-app.use(cors({credentials:true,origin:true}))
+// app.use(cors({credentials:true,origin:true}))
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({extended:true}))
 mongoose.connect('mongodb+srv://bharat:bharat@1188@cluster0.rr7at.mongodb.net/expressdb',{useNewUrlParser:true,useUnifiedTopology:true})
